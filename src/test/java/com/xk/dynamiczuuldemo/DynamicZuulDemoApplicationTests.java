31dcd663c4f4f845f4c4b052e9b00502d5384976
xk <>
2019-09-24 09:38:25 +0800

    zuul  nacos动态路由

---
+++ src/test/java/com/xk/dynamiczuuldemo/DynamicZuulDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xk.dynamiczuuldemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DynamicZuulDemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
